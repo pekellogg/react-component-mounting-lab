c608d042d97d8e2242cc577f7f1ca7ab444768b8
Peyton Kellogg <pekellogg@hotmail.com>
2023-01-13 16:36:44 -0800

    commit tree fefc3f8fc99730552821b13f81d54666184c10a5

--- src/App.js
+++ src/App.js
@@ -1,45 +1,38 @@
-import React, { Component } from 'react';
-
-import Timer from './Timer'
+import React, { Component } from "react";
+import Timer from "./Timer"
 
 class App extends Component {
 
-  //no props being used here, so we can use the shorthand declaration of state
   state = {
     timerIDs: []
   }
 
+  componentDidMount() {
+    console.log("called App.componentDidMount()");
+    this.handleAddTimer();
+  };
 
-  //Your code here:
-
-
-
-
-
-
-
-
-
-  // No need to modify anything in render or the class methods below
-  // Unless, of course, you're curious about how it all works
   render() {
-
     return (
       <div className="App">
         <h1>MultiTimer</h1>
         <button onClick={this.handleAddTimer}>Add New Timer</button>
-
         <div className="TimerGrid">
           {this.renderTimers()}
         </div>
-
       </div>
     );
   }
 
   // returns array of components written in JSX, mapped from this.state.timerIDs
   renderTimers = () => this.state.timerIDs.map(id => {
-    return <Timer key={id} id={id} removeTimer={this.removeTimer} />
+    return (
+      <Timer
+        key={id}
+        id={id}
+        removeTimer={this.removeTimer}
+      />
+    );
   })
 
   // adds a random number for timer ID
@@ -55,7 +48,6 @@ class App extends Component {
       timerIDs: prevState.timerIDs.filter(timer_id => timer_id !== id)
     }))
   }
-
 
 }
 
--- src/Timer.js
+++ src/Timer.js
@@ -1,15 +1,22 @@
 import React, { Component } from "react";
 
 class Timer extends Component {
+
   state = {
     time: 0,
     color: "#" + Math.floor(Math.random() * 16777215).toString(16)
   };
 
-  // add your code here
+  componentDidMount() {
+    this.interval = setInterval(this.clockTick, 1000);
+  };
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
 
   render() {
-    const { time, color } = this.state;
+    const { time, color, className } = this.state;
     return (
       <section className="Timer" style={{ background: color }}>
         <h1>{time}</h1>
@@ -20,21 +27,15 @@ class Timer extends Component {
     );
   }
 
-  //clock functions
   clockTick = () => {
     this.setState(prevState => ({
       time: prevState.time + 1
-    }));
-  };
+    }))
+  }
 
-  stopClock = () => {
-    clearInterval(this.interval);
-  };
+  stopClock = () => clearInterval(this.interval);
 
-  // for the 'x' button,
-  handleClose = () => {
-    this.props.removeTimer(this.props.id);
-  };
+  handleClose = () => this.props.removeTimer(this.props.id);
 }
 
 export default Timer;
